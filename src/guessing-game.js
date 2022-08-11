6af859a2c8e6463f91b4324742f2a9722f0ff381
pranatoli <pr.anatoli@gmail.com>
2022-08-11 12:25:33 +0300

    init: do task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.guessNum = Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = this.guessNum;
+        this.guess();
     }
 
     greater() {
-
+        this.min = this.guessNum;
+        this.guess();
     }
 }
 
